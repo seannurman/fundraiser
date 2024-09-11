52540df9d3cd435f52fd9715e1f2c7cd82e4e11f
seannurman <seannurman@gmail.com>
2024-09-11 16:25:03 +0600

    Update URL

--- TESTURL/custom.js
+++ TESTURL/custom.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const cart = [];
-    const baseUrl = "/?form=FUNKUZGBBCU";
+    const baseUrl = "https://seannurman.github.io/fundraiser/TESTURL/?form=FUNKUZGBBCU";
 
     const products = [
         {
