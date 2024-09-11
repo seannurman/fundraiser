ef22a8eeeb434a0f6e2b0ba841d64f0ea7506e08
seannurman <seannurman@gmail.com>
2024-09-11 16:53:40 +0600

    dsa

--- TESTURL/custom.js
+++ TESTURL/custom.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const cart = [];
-    const baseUrl = "https://seannurman.github.io/fundraiser/TESTURL/?form=FUNHNWFXQFW";
+    const baseUrl = "https://seannurman.github.io/fundraiser/TESTURL/?form=FUNHNWFXQFWS";
 
     const products = [
         {
